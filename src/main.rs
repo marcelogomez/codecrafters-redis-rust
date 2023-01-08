c10d57f2c4cad15f81941cfcbb317c1013e51b1a
Marcelo Gomez <marcelogomez321@gmail.com>
2023-01-08 22:21:03 +0000

    implement set command

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,33 @@
 use redis_starter_rust::BulkString;
 use redis_starter_rust::RESPValue;
+use std::collections::HashMap;
 use std::convert::TryInto;
 #[allow(unused_imports)]
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
 use std::net::SocketAddr;
+use std::sync::Arc;
+use std::sync::RwLock;
 use tokio::io::AsyncReadExt;
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
+
+type Table = Arc<RwLock<HashMap<String, String>>>;
 
 #[tokio::main]
 async fn main() {
     // Uncomment this block to pass the first stage
     let listener = TcpListener::bind("0.0.0.0:6379").await.unwrap();
     let mut connections = vec![];
+    let table = Arc::new(RwLock::new(HashMap::new()));
+
     // TODO: Handle accept errors
     while let Ok((socket, addr)) = listener.accept().await {
+        let table = Arc::clone(&table);
         connections.push(tokio::task::spawn(async move {
-            handle_client(socket, addr).await
+            handle_client(socket, addr, table).await
         }));
     }
 
@@ -39,7 +47,7 @@ async fn main() {
     //     .await;
 }
 
-async fn handle_client(mut socket: TcpStream, addr: SocketAddr) {
+async fn handle_client(mut socket: TcpStream, addr: SocketAddr, table: Table) {
     eprintln!("Connected to client {}", addr);
     let mut command_buf = [0u8; 4096];
     loop {
@@ -48,7 +56,7 @@ async fn handle_client(mut socket: TcpStream, addr: SocketAddr) {
                 eprintln!("Connection terminated by client {}", addr);
                 break;
             }
-            Ok(n) => match handle_command(&command_buf[..n]) {
+            Ok(n) => match handle_command(&command_buf[..n], table.clone()) {
                 Ok(resp) => {
                     socket.write(resp.as_bytes()).await.unwrap();
                     socket.flush().await.unwrap();
@@ -65,7 +73,7 @@ async fn handle_client(mut socket: TcpStream, addr: SocketAddr) {
     }
 }
 
-fn handle_command(command_buf: &[u8]) -> Result<String, String> {
+fn handle_command(command_buf: &[u8], table: Table) -> Result<String, String> {
     let (resp_value, _) = RESPValue::parse(command_buf).map_err(|e| format!("{:?}", e))?;
     // TODO: Make this easier
     let command: Option<Vec<BulkString>> = resp_value.try_into().unwrap();
@@ -75,10 +83,14 @@ fn handle_command(command_buf: &[u8]) -> Result<String, String> {
         return Err("Empty command".into());
     }
 
-    Ok(gen_response(command[0].as_ref().unwrap(), &command[1..])?)
+    Ok(gen_response(
+        command[0].as_ref().unwrap(),
+        &command[1..],
+        table,
+    )?)
 }
 
-fn gen_response(command: &String, args: &[BulkString]) -> Result<String, String> {
+fn gen_response(command: &String, args: &[BulkString], table: Table) -> Result<String, String> {
     match command.as_str() {
         "ECHO" | "echo" => {
             if args.is_empty() {
@@ -87,6 +99,30 @@ fn gen_response(command: &String, args: &[BulkString]) -> Result<String, String>
             // TODO: Make this easier
             let message = args[0].as_ref().map(|s| s.as_str()).unwrap_or_default();
             Ok(format!("${}\r\n{}\r\n", message.len(), message))
+        }
+        "SET" | "set" => {
+            let mut args = args.into_iter();
+            // TODO: Make this easier
+            let key = args
+                .next()
+                .and_then(|s| s.as_deref())
+                .ok_or_else(|| "No key specified for set operation".to_string())?;
+            let value = args
+                .next()
+                .and_then(|s| s.as_deref())
+                .ok_or_else(|| "No key specified for set operation".to_string())?;
+            
+            match table.write() {
+                Ok(mut t) => {
+                    t.insert(key.to_string(), value.to_string());
+                    Ok(format!("+OK\r\n"))
+                }
+                Err(e) => {
+                    eprintln!("Failed to acquire lock for table {}", e);
+                    // TODO: Make error handling simpler
+                    Ok(format!("$-1\r\n"))
+                }
+            }
         }
         "PING" | "ping" => Ok("+PONG\r\n".to_string()),
         c => Err(format!("Unknown command {}", c)),
